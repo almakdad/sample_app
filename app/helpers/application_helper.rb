ed3d282c496085b34d381f5209ce62c4e4dc416b
Mac Alnakari <malnakari@gmail.com>
2011-04-06 22:49:20 -0400

    Refactoring

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,5 @@
 module ApplicationHelper
+  
   # Return a title on a per-page basis.
   def title
     base_title = "My 1st Rails App"
@@ -8,4 +9,10 @@ module ApplicationHelper
       "#{base_title} | #{@title}"
     end
   end
+  
+  # Return a title on a per-page basis.
+  def logo
+    logo = image_tag("logo.png", :alt => "Sample App", :class => "round")
+  end  
+  
 end
