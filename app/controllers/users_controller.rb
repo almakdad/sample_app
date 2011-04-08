ac309b16e57d2ba16637da230d9dce813e12b0e5
Mac Alnakari <malnakari@gmail.com>
2011-04-08 17:05:17 -0400

    Finished first cut of the User model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,11 @@
 class UsersController < ApplicationController
 
+  def show
+    @user = User.find(params[:id])
+  end
+
   def signup
     @title= "Sign up"  
-  end
+  end  
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 SampleApp::Application.routes.draw do
   
+  resources :users
+  
   match '/', :to => 'pages#home'
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
