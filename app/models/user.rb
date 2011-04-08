12f36c16d27d72b317581857de34140087da7b77
Mac Alnakari <malnakari@gmail.com>
2011-04-08 18:06:23 -0400

    Stopping point pick up soon

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,8 @@
 #
 
 class User < ActiveRecord::Base
-  attr_accessible :name, :email
+  attr_accessor :password
+  attr_accessible :name, :email, :password, :password_confirmation
 
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
@@ -20,4 +21,38 @@ class User < ActiveRecord::Base
   validates :email, :presence => true,
                     :format   => { :with => email_regex },
                     :uniqueness => { :case_sensitive => false }
+                    
+  # Automatically create the virtual attribute 'password_confirmation'.
+  validates :password, :presence     => true,
+                       :confirmation => true,
+                       :length       => { :within => 6..40 }
+                       
+   before_save :encrypt_password
+
+   # Return true if the user's password matches the submitted password.
+    def has_password?(submitted_password)
+      # Compare encrypted_password with the encrypted version of
+      # submitted_password.
+      encrypted_password == encrypt(submitted_password)
+    end
+    
+   private
+
+     def encrypt_password
+       self.salt = make_salt if new_record?
+       self.encrypted_password = encrypt(password)
+     end
+
+     def encrypt(string)
+       secure_hash("#{salt}--#{string}")
+     end
+     
+     def make_salt
+       secure_hash("#{Time.now.utc}--#{password}")
+     end
+
+     def secure_hash(string)
+       Digest::SHA2.hexdigest(string)
+     end      
+                       
 end
